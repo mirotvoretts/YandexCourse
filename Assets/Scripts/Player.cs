e7f0bf315837f1977e13cc6c2550f7479b29a7ac
mirotvoretts <panoffilya@gmail.com>
2023-02-13 20:02:52 +0500

    Л2 Задача №1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private HingeJoint2D _joint;
+    private void Start()
+    {
+        _joint = GetComponent<HingeJoint2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            UnhookFromRope();
+        }
+    }
+    
+    private void UnhookFromRope()
+    {
+        _joint.enabled = false;
+    }
+}
